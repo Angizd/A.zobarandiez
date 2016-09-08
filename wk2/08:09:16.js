812ac61ad3ccd795cda8703f69d6695dc738cae4
angi <angi@angis-MacBook-Pro.local>
2016-09-08 17:30:24 +0200

    canvas

---
+++ wk2/08:09:16.js
@@ -0,0 +1,18 @@
+
+/*Write a function that expects a string representing a class name to be passed as a parameter.
+The function should return an array containing all
+the elements in the document that have the class that was passed in.
+This function should not use getElementsByClassName or querySelectorAll.
+*/
+
+function getClass(){
+  var x= document.getElementsByTagName("getClass").length;
+};
+console.log(getClass);
+
+var arr=[]
+for (var i =0; i < getClass; i++) {
+    arr.push(i);
+
+}
+console.log(arr);
