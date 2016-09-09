ba8957f4698211885765e7b981c1c505af2bc4e7
angi <angi@angis-MacBook-Pro.local>
2016-09-09 09:52:04 +0200

    finish

---
+++ wk2/wednesday.js
@@ -0,0 +1,21 @@
+var num=
+var doublenum=
+
+num(3)
+
+function timesTwo(3){
+   return num*2
+    };
+    
+timestwo(3){
+   return doublenum}
+   
+   
+var numbers= [num, doublenum];
+   for([i]=0, [i]< num.length, i++){
+   timestwo(doublenum);
+   
+numbers()
+
+numbers(num("doublenum");
+   
